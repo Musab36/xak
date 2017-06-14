bfc270cbb13077f079efc83e942e2f7dfbc8aa62
Musab36 <musabhd@gmail.com>
2017-06-15 02:00:48 +0300

    add index.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".fa-search").click(function() {
+    $(".fa-search").hide();
+    $(".btn").hide();
+    $(".btnsr").show();
+    $(".input").show();
+  });
+});
