a6dd0c62a622aaa1990f4b121d3c9787669d1b5b
Musab36 <musabhd@gmail.com>
2017-06-16 23:04:54 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
-  $(".fa-search").click(function() {
+  $(".fa-search").click(function(event) {
     $(".fa-search").hide();
     $(".btn").hide();
     $(".btnsr").show();
     $(".input").show();
+    event.preventDefault();
   });
+  $("#mor1").click(function(event) {
+    $("#m1").show();
+    $("#container2").show();
+    $(".all").hide();
+    $("#container1").hide();
+    event.preventDefault();
+  });
+  $("#home").click(function(event) {
+    $(".all").show();
+    $("#m1").hide();
+    $("#container1").show();
+    $("#container2").hide();
+    event.preventDefault();
+  });
+  $('#m1').submit(function(event){
+  event.preventDefault();
+  var post = $('.comment').val();
+  $("#comment-list").append('<div> User: ' + post +  '</div>');
+});
 });
