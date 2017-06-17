a38822a20a466fe89d7e9fb466959e85159420d1
Musab36 <musabhd@gmail.com>
2017-06-18 01:43:07 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
     event.preventDefault();
   });
   $("#mor1").click(function(event) {
-    $("#m1").show();
-    $("#container2").show();
+    $("#m1").fadeIn();
+    $("#container2").fadeIn();
     $(".all").hide();
     $("#container1").hide();
     event.preventDefault();
@@ -20,12 +20,21 @@ $(document).ready(function() {
     $("#container1").fadeOut();
     $("#container2").fadeOut();
   });
+  $("#mor3").click(function() {
+    $("#container4").fadeIn();
+    $(".all").fadeOut();
+    $("#container1").fadeOut();
+    $("#container2").fadeOut();
+    $("#container3").fadeOut();
+  });
+
   $("#home").click(function(event) {
     $(".all").show();
     $("#m1").hide();
     $("#container1").show();
     $("#container2").hide();
     $("#container3").fadeOut();
+    $("#container4").fadeOut();
     event.preventDefault();
   });
   $('#m1').submit(function(event){
@@ -46,6 +55,11 @@ $(".sign").click(function(event) {
    event.preventDefault();
    var postTwo = $(".comment2").val();
    $("#comment-list2").append('<li> User: ' + postTwo + '</li>');
+ });
+ $("#m3").submit(function(event) {
+   event.preventDefault();
+   var postThree = $(".comment3").val();
+   $("#comment-list3").append('<li> User: ' + postThree + '</li>');
  });
  $(".fa-thumbs-up").click(function(event) {
    $("#down").fadeOut();
@@ -77,9 +91,22 @@ $(".sign").click(function(event) {
    $(".share").fadeIn();
    event.preventDefault();
  });
+ $(".fa-thumbs-up").click(function(event) {
+   $("#down3").fadeOut();
+   $("#up3").fadeIn();
+   document.getElementById('up3').innerHTML = ("You liked this story!");
+   event.preventDefault();
+ });
+ $(".fa-thumbs-down").click(function(event) {
+   $("#up3").fadeOut();
+   $("#down3").fadeIn();
+   document.getElementById('down3').innerHTML = ("You disliked this story!");
+   event.preventDefault();
+ });
  $(".delete").click(function() {
    $("#comment-list").fadeOut();
    $("#comment-list2").fadeOut();
+   $(".post-wells").fadeOut();
 
  });
  $(".form-btn").click(function() {
